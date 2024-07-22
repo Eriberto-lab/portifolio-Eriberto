2387810314603dca69ec3c27e83fdd3fe9225fe9
Eriberto Lima <eribertosflima@gmail.com>
2024-07-22 11:56:11 -0300

    "feat:  adiciona sessão de contato"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Contact from './sections/Contact/Contact'
 import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
 import Hero from './sections/hero/Hero'
@@ -12,6 +13,7 @@ function App() {
      <Hero/>
      <Projects/>
      <Skills/>
+     <Contact />
     </>
   )
 }
---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,47 @@
+import styles from "./ContactStyles.module.css";
+
+function Contact() {
+  return (
+    <section id="contect" className={styles.container}>
+      <h1 className="sectionTitle">Contact me</h1>
+      <form action="">
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            message
+          </label>
+          <textarea name="message" id="message" placeholder="Message" required>
+            {" "}
+          </textarea>
+        </div>
+        <input type="submit" className="hover btn" submit value="Submit" />
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
